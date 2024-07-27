2aa97aa59815fd07f18ca2389152efbe5985aa89
1375626371 <1375626371@qq.com>
2024-07-27 19:32:46 +0800

    更新了下载函数
    更新了下载函数

--- pdf_download.py
+++ pdf_download.py
@@ -1,3 +1,5 @@
+import traceback
+
 import arxiv
 import pymongo
 from config import config
@@ -12,16 +14,17 @@ from pdf_check import  check_pdf_integrity
 import os
 import requests
 import sys
+session = requests.session()
+session.headers ={
+        "referer": "https://www.baidu.com/",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+}
 myclient = pymongo.MongoClient(config.mongo_client_url)
 mydb = myclient["paper_connect"]
 db_data = mydb["data"]
 save_path = config.save_path
 count=1926856
 pool_num=61
-headers = {
-        "referer": "https://www.baidu.com.com/",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
-    }
 def download_pdf(arxiv_data):
     # arxiv_data=db_data.find_one({"downloaded":{'$exists':False}})
     # random_index = random.randint(0, count - 1)
@@ -34,20 +37,46 @@ def download_pdf(arxiv_data):
         pdf_path = save_path+date+'/'+entey+'.pdf'
         pdf_path_temp = save_path+date+'/'+entey+'_temp.pdf'
         pdf_url = arxiv_data['pdf_url']#.replace("http://arxiv.org/","http://xxx.itp.ac.cn/")
-        response = requests.get(pdf_url,headers=headers)
+        try:
+            response = session.get(pdf_url)
+        except:
+            traceback.print_exc()
+            return
         # if  os.path.exists(pdf_path):
         #     return
         try:
             if len(response.content)<10000 and "arXiv reCAPTCHA" in response.text:
                 print("reCAPTCHA")
                 sys.exit(0)
+            if response.status_code == 404:
+                if 'v' in pdf_url.split('.')[-1] and int(pdf_url.split('.')[-1].split('v')[-1])>1:
+                    url_no_v=pdf_url[:pdf_url.rfind('v')]
+                    version=int(pdf_url.split('.')[-1].split('v')[-1])
+                    pdf_url=f'{url_no_v}v{version-1}'
+                    entey_id=f"{arxiv_data['entry_id'][:arxiv_data['entry_id'].rfind('v')]}v{version-1}"
+                    db_data.update_one({"entry_id":arxiv_data['entry_id']},{"$set":{"pdf_url":pdf_url,"entry_id":entey_id}})
+                    try:
+                        response = session.get(pdf_url)
+                    except:
+                        traceback.print_exc()
+                        return
+                else:
+                    db_data.update_one({"entry_id": arxiv_data['entry_id']}, {"$set": {"arxiv_pdf_error": True}})
+                    return
             if response.status_code != 200:
-                time.sleep(4)
                 print(pdf_url,"status_code:",response.status_code)
+                if response.status_code == 403:
+                    sys.exit(0)
+                if response.status_code // 100 == 5:
+                    db_data.update_one({"entry_id":arxiv_data['entry_id']},{"$set":{"arxiv_pdf_error":True}})
+                time.sleep(4)
                 return
             with open(pdf_path, 'wb') as f:
                 f.write(response.content)
-            compress_pdf(pdf_path, pdf_path_temp)
+            try:
+                compress_pdf(pdf_path, pdf_path_temp)
+            except:
+                traceback.print_exc()
             if check_pdf_integrity(pdf_path_temp):
                 os.remove(pdf_path)
                 os.rename(pdf_path_temp,pdf_path)
@@ -93,9 +122,8 @@ if __name__ == "__main__":
     
     # 指定进程数和每个进程的线程数
     num_processes = 1
-    num_threads = 1
-    
-    process_with_pools([i for i in db_data.find({"downloaded": False,"primary_category":{"$regex": "^cs"}})], num_processes, num_threads)
+    num_threads = 10
+    process_with_pools([i for i in db_data.find({"downloaded": False,"primary_category":{"$regex": "^cs"},"arxiv_pdf_error":False})], num_processes, num_threads)
 
 
 
