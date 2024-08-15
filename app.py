09764eb1256032edf6e10cd804b9e67711924220
yaoqih <1375626371@qq.com>
2024-08-15 22:36:40 +0800

    添加了推广 添加了压缩

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify,send_from_directory
+from flask_compress import Compress
+
+import os
 from query import find_neighbors,collection
 app = Flask(__name__)
+Compress(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/favicon.ico')#设置icon
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),#对于当前文件所在路径,比如这里是static下的favicon.ico
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form['query']
